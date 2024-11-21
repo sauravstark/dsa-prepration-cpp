5730a08c525549eb5543dccd587b14c1734ea611
Saurav Stark <saurav.stark15@gmail.com>
2024-11-21 05:47:37 +0530

    LC940_DistinctSubsequencesII

---
+++ tests/leetcode/problems/LC940_DistinctSubsequencesII.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include <leetcode/problems/LC940_DistinctSubsequencesII.h>
+#include <leetcode/util/TastcaseReader.h>
+
+TEST(LeetCode, LC940_DistinctSubsequencesII) {
+    LeetCode::Util::TestcaseReader testcaseReader("testcases/leetcode/LC940_DistinctSubsequencesII.txt", 2);
+
+    while (testcaseReader.hasNext()) {
+        const auto words = testcaseReader.next();
+        const auto& input = words[0];
+        const auto expected = std::stoi(words[1]);
+        const auto result = LeetCode::Problems::LC940::distinctSubseqII(input);
+        EXPECT_EQ(expected, result);
+    }
+}
