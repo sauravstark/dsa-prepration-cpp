6f7ff3b6da3a61f11e054e5b1e7de0218a69f904
Saurav Stark <saurav.stark15@gmail.com>
2024-11-21 06:30:12 +0530

    LC2458_HeightOfBinaryTreeAfterSubtreeRemovalQueries

---
+++ tests/leetcode/problems/LC2458_HeightOfBinaryTreeAfterSubtreeRemovalQueries.cpp
@@ -0,0 +1,27 @@
+#include <common/util/String.h>
+#include <gtest/gtest.h>
+#include <leetcode/datatype/TreeNode.h>
+#include <leetcode/problems/LC2458_HeightOfBinaryTreeAfterSubtreeRemovalQueries.h>
+#include <leetcode/util/TastcaseReader.h>
+
+TEST(LeetCode, LC2458_HeightOfBinaryTreeAfterSubtreeRemovalQueries) {
+    LeetCode::Util::TestcaseReader testcaseReader("testcases/leetcode/LC2458_HeightOfBinaryTreeAfterSubtreeRemovalQueries.txt", 3);
+
+    while (testcaseReader.hasNext()) {
+        const auto words = testcaseReader.next();
+        const auto input = Common::Util::String::parseVectorString(words[0]);
+        const auto queries = Common::Util::String::parseVectorInt(words[1]);
+        const auto expectedResult = Common::Util::String::parseVectorInt(words[2]);
+        const auto inputTree = LeetCode::DataTypes::TreeNode::buildTreeFromVector(input);
+
+        if (queries.size() != expectedResult.size()) {
+            throw std::invalid_argument("Number of queries does not match number of results");
+        }
+
+        const auto result = LeetCode::Problems::LC2458::treeQueries(inputTree, queries);
+
+        ASSERT_EQ(expectedResult, result);
+
+        LeetCode::DataTypes::TreeNode::deleteTree(inputTree);
+    }
+}
